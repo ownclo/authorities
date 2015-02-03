4de0690e4d4bd17266d2d37e5b493e4c4c59b540
Ownclo - Arcis <mrownclo@gmail.com>
2015-02-03 10:33:43 +0300

    Generalized (almost) many-to-many queries

--- Authorities/Join.hs
+++ Authorities/Join.hs
@@ -2,12 +2,14 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Authorities.Join where
 
 import Database.Esqueleto
 
 type EntityExpr a = SqlExpr (Entity a)
+type JoinExpr a b = (EntityExpr a, EntityExpr b)
 
 -- TODO: Is it possible to generalize 'groupPersonJoin'
 -- in order to support all pairs of tables joined in that way?
@@ -19,7 +21,8 @@ class (SqlEntity right, SqlEntity lr)
       => RightJoined lr right where
     rightId :: EntityField lr (Key right)
 
-class (LeftJoined left lr, RightJoined lr right)
+class (LeftJoined left lr,
+       RightJoined lr right)
       => Joined left lr right where
 
 join :: (Joined left lr right)
@@ -32,3 +35,13 @@ join selector (left `InnerJoin` lr `InnerJoin` right) = do
     on (right ^. persistIdField ==. lr ^. rightId)
     on (left  ^. persistIdField ==. lr ^. leftId)
     selector left lr right
+
+manyToMany :: (Joined left lr right)
+           => Entity left
+           -> (EntityExpr left `InnerJoin` EntityExpr lr `InnerJoin` EntityExpr right)
+           -> SqlQuery (JoinExpr right lr)
+manyToMany (Entity key _) = join $
+    \first (jTable :: EntityExpr lr) second -> do
+    match first key persistIdField -- undefined -- leftId
+    return (second, jTable)
+  where match entity aKey anId = where_ (entity ^. anId ==. val aKey)
--- Authorities/Queries.hs
+++ Authorities/Queries.hs
@@ -2,7 +2,7 @@
 module Authorities.Queries where
 
 import Authorities.Schema
-import Authorities.Join( join, EntityExpr )
+import Authorities.Join
 import Database.Esqueleto
 
 -- NOTE: that '_gp :: EntityExpr PersonGroup' is used
@@ -11,16 +11,17 @@ import Database.Esqueleto
 -- TODO: That join query is still too repetitive. Abstract
 -- that out if possible.
 groupsForPerson :: Entity Person -> SqlQuery (EntityExpr Group)
-groupsForPerson (Entity pkey _) = from . join $
-    \person (_pg :: EntityExpr PersonGroup) group -> do
-    where_ (person ^. PersonId ==. val pkey)
-    return group
+groupsForPerson p = fst `fmap` groupsForPerson' p
+
+groupsForPerson' :: Entity Person -> SqlQuery (JoinExpr Group PersonGroup)
+groupsForPerson' = from . manyToMany
 
 peopleInGroup :: Entity Group -> SqlQuery (EntityExpr Person)
 peopleInGroup (Entity gkey _) = from . join $
     \person (_pg :: EntityExpr PersonGroup) group -> do
-    where_ (group  ^. GroupId ==. val gkey)
+    match group gkey GroupId
     return person
+  where match entity key anId = where_ (entity ^. anId ==. val key)
 
 authoritiesForGroup :: Entity Group -> SqlQuery (EntityExpr Authority)
 authoritiesForGroup (Entity gkey _) = from . join $
